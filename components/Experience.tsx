31fae183249e3a3612813266651a24b1ceacabaa
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-08-29 14:12:45 -0400

    deployemnt changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -6,7 +6,7 @@ const Experience = () => {
     <div className='py-20' id='experience'>
             <h1 className="heading">
         My
-        <span className="text-purple"> word experience</span>
+        <span className="text-purple"> work experience</span>
       </h1>
       <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
         {workExperience.map((card) => (
